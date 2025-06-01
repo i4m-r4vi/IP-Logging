c2a85cb827ff75621f9046faf05a4902fdfeeb82
Ravi <ayswarym04@gmail.com>
2025-06-01 19:46:48 +0530

    Some Changed

--- index.js
+++ index.js
@@ -8,19 +8,8 @@ app.get('/', (req, res) => {
   const interfaces = os.networkInterfaces();
   const ip = [];
 
-  for (const name in interfaces) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        ip.push(iface.address);
-      }
-    }
-  }
+  console.log(interfaces['Wi-Fi'])
 
-  if (ip.length === 0) {
-    return res.json({ message: 'No local IP addresses found' });
-  }
-
-  console.log({ localIPs: ip });
   res.status(200).send("Welcome")
 });
 
